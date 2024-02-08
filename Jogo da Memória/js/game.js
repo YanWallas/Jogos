c53d969111d33eb9931b1255e9050427802f80d1
yanwallas <yanwallas98@gmail.com>
2024-02-08 11:51:24 -0300

    Create Card

---
+++ Jogo da Memória/js/game.js
@@ -0,0 +1,46 @@
+const grid = document.querySelector('.grid');// buscando a div com a class grid.
+
+const caracters = [
+  'gojo',
+  'goku',
+  'itachi',
+  'kakashi',
+  'luffy',
+  'nami',
+  'naruto',
+  'pika',
+  'sasuke',
+  'um',
+  'vegeta',
+  'zoro',
+];
+
+const createElement = (tag, className) => {// função que cria o elemento
+  const element = document.createElement(tag);
+  element.className = className;
+  return element;
+}
+
+const createCard = (caracter) => {
+  const card = createElement('div', 'card');//criando a div card com a class.
+  const front = createElement('div', 'face front');;//criando a div front com a class.
+  const back = createElement('div', 'face back');;//criando a div back com a class.
+
+  front.style.backgroundImage = `url('../img/${caracter}.png')`;
+
+  card.appendChild(front);//colocando como filha(dentro) da div card.
+  card.appendChild(back);//colocando como filha(dentro) da div card.
+
+  return card;
+}
+
+const loadGame = () => {
+  const duplicateCaracters = [ ...caracters, ...caracters ]
+
+  duplicateCaracters.forEach((caracter) => {
+    const card = createCard(caracter);
+    grid.appendChild(card);
+  });
+}
+
+loadGame();
--- Jogo da Memória/js/login.js
+++ Jogo da Memória/js/login.js
@@ -2,24 +2,24 @@ const input = document.querySelector('.login_input');
 const button = document.querySelector('.login_button');
 const form = document.querySelector('.login-form');
 
-const validateInput = ({ target }) => {
-  if (target.value.length > 2) {
-    button.removeAttribute('disabled');
+const validateInput = ({ target }) => { //pegando o target(o que tem dentro dentro do input)
+  if (target.value.length > 2) {//target(o que tem dentro do input).valor do target. tamanho maior que 2.
+    button.removeAttribute('disabled');//Vai remove o atrribute desativado. 
     return;
   }
     
-  button.setAttribute('disabled', '');
+  button.setAttribute('disabled', '');//se o tamanho digitado for menor que 2, ai ficar desativado o button.
 }
 
 const handleSubmit = (event) => {
   event.preventDefault();
 
-  localStorage.setItem('player', input.value);
-  window.location = 'pages/game.html';
+  localStorage.setItem('player', input.value);//Onde vai salva o nome.
+  window.location = 'pages/game.html';//redirecionamento da pagina quando clicar no botão
 
 }
 
-input.addEventListener('input', validateInput);
-form.addEventListener('submit', handleSubmit);
+input.addEventListener('input', validateInput);//pegando o evento de digitar o nome.
+form.addEventListener('submit', handleSubmit);//pegando o evento de clicar no button.
 
 
